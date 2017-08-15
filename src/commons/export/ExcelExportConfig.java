24a653397a428c88a4d5881e4f709c2b8864f880
yhao <yhao@yhao>
2017-08-15 21:04:52 +0800

    export

--- src/commons/export/ExcelExportConfig.java
+++ src/commons/export/ExcelExportConfig.java
@@ -13,7 +13,6 @@ import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
