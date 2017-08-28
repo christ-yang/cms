889bdc3a46d76d4c341d65b5ccfbf416c4eadce6
yhao <yhao@yhao>
2017-08-28 14:19:24 +0800

    excel

--- src/me/yhao/export/ExportTest.java
+++ src/me/yhao/export/ExportTest.java
@@ -1,9 +1,13 @@
 package me.yhao.export;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +19,7 @@ import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 public class ExportTest {
 
@@ -83,5 +88,34 @@ public class ExportTest {
 		}
 	}
 	
+	
+	public String[][] getDate(File file, int ignoreRows) throws IOException {
+		List<String> result = new ArrayList<>();
+		int rowSize = 0;
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+		//打开HSSFWorkbook
+		POIFSFileSystem fs = new POIFSFileSystem();
+		HSSFWorkbook wb = new HSSFWorkbook(fs);
+		HSSFCell cell = null;
+		for (int sheetIndex = 0; sheetIndex < wb.getNumberOfSheets(); sheetIndex++) {
+			HSSFSheet st = wb.getSheetAt(sheetIndex);
+			//ignorerows行 不取
+			for (int rowIndex = ignoreRows; rowIndex < st.getLastRowNum(); rowIndex++) {
+				HSSFRow row = st.getRow(rowIndex);
+				if (row == null) {
+					continue;
+				}
+				int tempRowSize = row.getLastCellNum()+1;
+				if (tempRowSize > rowSize) {
+					rowSize = tempRowSize;
+				}
+				String[] values = new String[rowSize];
+				Arrays.fill(values, "");
+				boolean hasvalue = false;
+				
+			}
+		}
+		return null;
+	}
 
 }
